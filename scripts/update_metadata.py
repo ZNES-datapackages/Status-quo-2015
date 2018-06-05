0b55abcf735c82ade34d05a87fcbc67746cc947f
Martin <martin.soethe@uni-flensburg.de>
2018-06-05 14:08:59 +0200

    Add package name and profile

--- scripts/update_metadata.py
+++ scripts/update_metadata.py
@@ -4,7 +4,12 @@
 import os
 from datapackage import Package, Resource
 
-p = Package()
+descriptor = {}
+descriptor['name'] = 'Status quo 2015'
+descriptor['profile'] = 'tabular-data-package'
+
+# create Package based on infer above
+p = Package(descriptor)
 
 for f in os.listdir('resources'):
     path = os.path.join('resources', f)
