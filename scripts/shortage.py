f1a0797debf04cc5fb467d20753615ed1ea232b0
Martin <martin.soethe@uni-flensburg.de>
2018-11-20 13:27:53 +0100

    Rename and split slack script

---
+++ scripts/shortage.py
@@ -0,0 +1,21 @@
+"""
+"""
+
+import pandas as pd
+
+from datapackage_utilities import building
+
+config = building.get_config()
+
+buses = building.read_elements('bus.csv')
+buses.index.name = 'bus'
+
+elements = pd.DataFrame(buses.index)
+
+elements['type'] = 'shortage'
+elements['name'] = 'shortage-' + elements['bus'].str[:2]
+elements['capacity'] = 50000
+elements['marginal_cost'] = 1000
+
+elements.set_index('name', inplace=True)
+building.write_elements('shortage.csv', elements)
