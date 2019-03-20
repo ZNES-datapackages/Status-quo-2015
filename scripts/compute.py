f90bb50703c85972f59b2309827842f59b73c335
Martin <martin.soethe@uni-flensburg.de>
2019-03-20 11:53:11 +0100

    Update compute script
    Include short summary of the results

--- scripts/compute.py
+++ scripts/compute.py
@@ -8,15 +8,17 @@ import pandas as pd
 from oemof.tabular import facades
 from oemof.tabular.datapackage import building
 from oemof.tabular.tools import postprocessing as pp
-from oemof.solph import EnergySystem, Model
+from oemof.solph import EnergySystem, Model, Bus
 
 """
 """
+
+timesteps = 8760
 
 for f in os.listdir('data/sequences/'):
     fname = os.path.join('data', 'sequences', f)
     df = pd.read_csv(fname, sep=';')
-    df = df.iloc[:8760]
+    df = df.iloc[:timesteps]
     df.to_csv(fname, index=False, sep=';')
 
 config = building.read_build_config('config.toml')
@@ -42,3 +44,31 @@ if os.path.exists('results'):
 os.mkdir('results')
 
 pp.write_results(m, 'results', scalars=False)
+
+# create short summary
+supply_sum = (
+    pp.supply_results(
+        results=m.results,
+        es=m.es,
+        bus=[b.label for b in es.nodes if isinstance(b, Bus)],
+        types=[
+            "dispatchable",
+            "volatile",
+            "reservoir",
+        ],
+    )
+    .sum()
+    .reset_index()
+)
+
+supply_sum['from'] = supply_sum['from'].apply(lambda i: '-'.join(i.label.split("-")[1:3:]))
+supply_sum = supply_sum.groupby(['from', 'to', 'type']).sum().reset_index()
+
+supply_sum.drop("type", axis=1, inplace=True)
+supply_sum = (
+    supply_sum.set_index(["from", "to"]).unstack("from")
+    / 1e6
+)
+
+supply_sum.columns = supply_sum.columns.droplevel(0)
+supply_sum.to_csv(os.path.join('results', 'summary.csv'))
